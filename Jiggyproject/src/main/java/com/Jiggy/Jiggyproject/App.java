3e8295b4d6262333009dfe6eec72b8b41dbbae98
bbabatop@gmail.com <Jbaba.bbabatop@gmail.com>
2024-03-18 22:30:11 -0400

    commit1

---
+++ Jiggyproject/src/main/java/com/Jiggy/Jiggyproject/App.java
@@ -0,0 +1,13 @@
+package com.Jiggy.Jiggyproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
